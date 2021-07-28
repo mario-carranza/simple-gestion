491fb5d8782e105e4db21d720ff568336720c141
Jose Alejandro <dluccajose@gmail.com>
2021-07-28 13:25:54 -0400

    change rrss links

--- resources/views/layouts/footer/menu.blade.php
+++ resources/views/layouts/footer/menu.blade.php
@@ -205,10 +205,10 @@
                 <div class="col-md-6 text-center text-md-right mb-4">
                     <div class="mb-3">
                         {{-- <a class="social-btn sb-light sb-twitter ml-2 mb-2" href="#"><i class="czi-twitter"></i></a> --}}
-                        <a class="social-btn sb-light sb-facebook ml-2 mb-2" href="https://www.facebook.com/camarachilenalibro/" target="_blank"><i class="czi-facebook"></i></a>
+                        <a class="social-btn sb-light sb-facebook ml-2 mb-2" href="https://www.facebook.com/Prolibro.cl" target="_blank"><i class="czi-facebook"></i></a>
                         <a class="social-btn sb-light sb-twitter ml-2 mb-2" href="https://twitter.com/camaradellibro" target="_blank"><i class="czi-twitter"></i></a>
-                        <a class="social-btn sb-light sb-instagram ml-2 mb-2" href="https://www.instagram.com/camaradellibro/" target="_blank"><i class="czi-instagram" target="_blank"></i></a>
-                        <a class="social-btn sb-light sb-youtube ml-2 mb-2" href="https://www.youtube.com/user/FILSAcamara/featured" target="_blank"><i class="czi-youtube" target="_blank"></i></a>
+                        <a class="social-btn sb-light sb-instagram ml-2 mb-2" href="https://www.instagram.com/prolibro/" target="_blank"><i class="czi-instagram" target="_blank"></i></a>
+                        <a class="social-btn sb-light sb-linkedin ml-2 mb-2" href="https://www.linkedin.com/company/prolibro/?viewAsMember=true" target="_blank"><i class="czi-linkedin" target="_blank"></i></a>
                         {{-- <a class="social-btn sb-light sb-pinterest ml-2 mb-2" href="#"><i class="czi-pinterest"></i></a>
                         <a class="social-btn sb-light sb-youtube ml-2 mb-2" href="#"><i class="czi-youtube"></i></a> --}}
                     </div>
