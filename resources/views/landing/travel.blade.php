1a3e0089ee997a160c6a2d052cd837e18f90dd5f
Jose Dlucca <jose.dlucca@twgroup.cl>
2021-12-02 18:05:29 -0400

    change travel landing

--- resources/views/landing/travel.blade.php
+++ resources/views/landing/travel.blade.php
@@ -1,10 +1,29 @@
 @extends('layouts.base')
 
+@push('styles')
+<style>
+    @media (min-width: 1000px) {
+        .travel-img {
+            width: 75%;
+        }
+    }
+</style>
+@endpush
+
 @section('content')
-<div class="row mt-5">
+<div class="row mt-5 mx-0 mx-md-5">
     <div class="col text-center">
         {{-- <i class="navbar-tool-icon czi-rocket" style="font-size: 150px; margin-top:50px; margin-bottom:100px"></i> --}}
-        <h1>Próximamente...</h1>
+        <div class="row">
+            <div class="col-12 col-md-6 mb-4 mb-md-0">
+                <a href="https://www.domosocoa.cl/reservas/">
+                    <img class="travel-img" src="{{ asset('img/turismo/domos-ocoa.jpg') }}">
+                </a>
+            </div>
+            <div class="col-12 col-md-6">
+                <img class="travel-img" src="{{ asset('img/turismo/tour-exp-campesina.jpg') }}" alt="">
+            </div>
+        </div>
         <img src="{{ asset('img/turismo_rural.png') }}" alt="">
     </div>
 </div>
