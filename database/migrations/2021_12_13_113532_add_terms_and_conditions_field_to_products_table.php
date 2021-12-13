24577f0569066af54a9eb62374a84602d45ce89c
Jose Dlucca <jose.dlucca@twgroup.cl>
2021-12-13 14:41:34 -0400

    added new fields to product

---
+++ database/migrations/2021_12_13_113532_add_terms_and_conditions_field_to_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTermsAndConditionsFieldToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->text('terms_and_conditions')->nullable()->after('template_id');
+            $table->decimal('price_factor', 8, 3)->nullable()->after('template_id');
+            $table->boolean('is_housing')->nullable()->after('template_id');
+            $table->text('housing_pricing')->nullable()->after('template_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('terms_and_conditions');
+            $table->dropColumn('price_factor');
+            $table->dropColumn('is_housing');
+            $table->dropColumn('housing_pricing');
+        });
+    }
+}
